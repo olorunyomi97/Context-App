c39ac8d940522d2c5ce8f0ab86bae083e5e1e909
olorunyomi97 <olorunyomi97@gmail.com>
2021-06-30 00:42:34 +0100

    useStae $ UUID

--- Projects/React Context Hooks/hooksapp/src/components/SongList.js
+++ Projects/React Context Hooks/hooksapp/src/components/SongList.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid from 'uuid/v1';
 
 const SongList = () => {
+    const [songs, setSongs] = useState([
+        {title: 'almost home', id: 1},
+        {title: 'bitches and bottles', id: 2},
+        {title: 'hallelujah', id: 3}
+    ]);
+    const addSong = () => {
+        setSongs([...songs, { title: 'Long as my bitches love me', id: uuid()}])
+    }
     return ( 
         <div className="song-list">
-            <li>Bigger Picture</li>
-            <li>A M A R I</li>
+           <ul>
+               {songs.map(song => {
+                   return (<li key={song.id}>{song.title}</li>);
+               })}
+           </ul>
+           <button onClick={addSong}>Add</button>
         </div>
      );
 }
