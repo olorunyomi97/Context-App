7b672b23f6cb973318e4b8985406de4b5fb5e8a7
olorunyomi97 <olorunyomi97@gmail.com>
2021-06-29 19:47:42 +0100

    hooks app

---
+++ Projects/React Context Hooks/hooksapp/src/components/SongList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SongList = () => {
+    return ( 
+        <div className="song-list">
+            <li>Bigger Picture</li>
+            <li>A M A R I</li>
+        </div>
+     );
+}
+ 
+export default SongList;
