f0e199340765dcef26fa5f1577b124092dee1f3b
olorunyomi97 <olorunyomi97@gmail.com>
2021-06-30 00:49:09 +0100

    Hooks and UUID

--- Projects/React Context Hooks/hooksapp/src/components/SongList.js
+++ Projects/React Context Hooks/hooksapp/src/components/SongList.js
@@ -5,7 +5,8 @@ const SongList = () => {
     const [songs, setSongs] = useState([
         {title: 'almost home', id: 1},
         {title: 'bitches and bottles', id: 2},
-        {title: 'hallelujah', id: 3}
+        {title: 'hallelujah', id: 3},
+        {title: '95 south', id: 4}
     ]);
     const addSong = () => {
         setSongs([...songs, { title: 'Long as my bitches love me', id: uuid()}])
